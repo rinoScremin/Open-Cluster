a3954a0b63a4569b4544c6333de11a2fcfda2209
rinoScremin <scremin49@gmail.com>
2026-01-20 07:37:26 -0500

    Update single_node_test.py

--- single_node_test.py
+++ single_node_test.py
@@ -3,8 +3,7 @@ from cluster_matrix_v1 import cluster_zmq
 from cluster_matrix_v1 import check_combined_result_values
 import torch
 import numpy as np
-
-#######################################------MAIN FUNCTION - SINLGE NODE TEST-----######################################  
+ 
 if __name__ == "__main__":
     
     A3_float16 = torch.from_numpy(np.random.rand(1500, 4500).astype(np.float16))
@@ -19,6 +18,7 @@ if __name__ == "__main__":
     matrix_pathA_float16 = 'model_matrices/small_matrixA_float16.pt'  
     matrix_pathB_float16 = 'model_matrices/small_matrixB_float16.pt'  
 
+    #######################################------SINLGE NODE TEST-----###################################### 
     cluster_zmq_obj = cluster_zmq(['192.168.2.100'])
     matrixA_single_node = cluster_matrix(
         matrix_file_path=matrix_pathA_float16,
@@ -30,7 +30,6 @@ if __name__ == "__main__":
         dim=0,
         auto_set_up=[1, "save"],
     )
-
     matrixB_single_node = cluster_matrix(
         matrix_file_path=matrix_pathB_float16,
         cluster_zmq_object=cluster_zmq_obj,
@@ -39,6 +38,30 @@ if __name__ == "__main__":
         back_end_select_list=["llama"],
         split_matrix=False,
         dim=0,
+        auto_set_up=[1, "save"],
+    )
+    matrixC_single_node = matrixA_single_node.cluster_shard_operation(
+        matrixB_single_node,
+        False,
+        True,
+        False ## IF YOU ARE USING 1 NODE DO NOT USE SEND BACK NOT NEEDED IT MUST BE SET TO FALSE FOR SINGLE NODE MATRIX OPERATION'S 
+    )
+    check_combined_result_values('model_matrices/c_ref_float16.pt',matrixC_single_node) # use the 'check_combined_result_values' function to make ssure
+
+    #######################################------SINLGE NODE TEST DEFULTS-----###################################### 
+
+    cluster_zmq_obj = cluster_zmq(['192.168.2.100'])
+    matrixA_single_node = cluster_matrix(
+        matrix_file_path=matrix_pathA_float16,
+        cluster_zmq_object=cluster_zmq_obj,
+        split_matrix=False,
+        auto_set_up=[1, "save"],
+    )
+
+    matrixB_single_node = cluster_matrix(
+        matrix_file_path=matrix_pathB_float16,
+        cluster_zmq_object=cluster_zmq_obj,
+        split_matrix=False,
         auto_set_up=[1, "save"],
     )
 
